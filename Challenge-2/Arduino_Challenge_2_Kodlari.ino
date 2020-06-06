c72865e308aa0264384a65187fa6d699c74903a6
Maker Atölye <65504952+maker-atolye@users.noreply.github.com>
2020-06-06 16:23:07 +0300

    Update Arduino_Challenge_2_Kodlari.ino

--- Challenge-2/Arduino_Challenge_2_Kodlari.ino
+++ Challenge-2/Arduino_Challenge_2_Kodlari.ino
@@ -3,16 +3,18 @@
 byte sayac = 0; //Buton kaçıncı kez basıldığını kontrol etmek için kullanacağımız değişken.
                 //Bu değişkene ya "1" ya da "2" değeri geleceğini bildiğimiz için veri tipini "byte" olarak belirledik.
                 //Eğer veri tipini "int"(integer) olarak tanımlamış olsaydık hafızada 2 kat daha fazla yer kaplamış olurdu.
+const int buton = 7;
+const int LED = 13;
 
 void setup() //Uygulama içinde tek seferlik işlemlerin yapılacağı bölüm.
 {
-  pinMode(7,INPUT); //7.pin giriş(INPUT) olarak ayarlandı.
-  pinMode(13,OUTPUT); //13.pin çıkış(OUTPUT) olarak ayarlandı.
+  pinMode(buton,INPUT); //7.pin giriş(INPUT) olarak ayarlandı.
+  pinMode(LED,OUTPUT); //13.pin çıkış(OUTPUT) olarak ayarlandı.
 }
 
 void loop() //Uygulama içinde sürekli yapılması istenen işlemlerin yapılacağı bölüm.
 {
-  byte gelen_deger = digitalRead(7); //digitalRead komutu ile butonun ürettiği değer okunup "gelen_deger" değişkenine gönderildi.
+  byte gelen_deger = digitalRead(buton); //digitalRead komutu ile butonun ürettiği değer okunup "gelen_deger" değişkenine gönderildi.
   if(gelen_deger == 1) //Eğer butondan gelen değer "1" ise sayac 1 arttırılacak. 
   {
     sayac++; //"sayac" değişkeni içindeki değer 1 arttırılır.
@@ -22,11 +24,11 @@ void loop() //Uygulama içinde sürekli yapılması istenen işlemlerin yapılac
   switch (sayac) //"sayac" değişkeni içindeki değerler "switch - case" yapısı ile kontrol edildi.
   {
     case 1: //Eğer "sayac" değişkeni içindeki değer "1" ise 13.pine bağlı olan LED yakılacak.
-    digitalWrite(13,1); //13.pine bağlı olan LED yakıldı.
+    digitalWrite(LED,1); //13.pine bağlı olan LED yakıldı.
     break; //İşlem sonlandırılır.
 
     case 2:  //Eğer "sayac" değişkeni içindeki değer "2" ise 13.pine bağlı olan LED söndürülecek.
-    digitalWrite(13,0); //13.pine bağlı olan LED söndürüldü.
+    digitalWrite(LED,0); //13.pine bağlı olan LED söndürüldü.
     sayac = 0; //"sayac" değişkeninin kontrolü sadece 1. ve 2. basışlara göre kontrol edildiği için ve daha fazla artmasını önlemek amacıyla "sayac" değişkeni içindeki değer sıfırlandı.
     break; //İşlem sonlandırılır.
   }
